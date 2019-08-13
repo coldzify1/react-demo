2ba093ccdd83319a42c6a8467e290cb8d9a70b44
kew <q@degitobangkok.com>
2019-08-13 10:15:52 +0700

    Updates

---
+++ precache-manifest.0553d58acc93f4e8cb9b33f1c6b16216.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20db9c1963f48d91b4e607888238fd78",
+    "url": "/coldzify1/react-demo/index.html"
+  },
+  {
+    "revision": "7f07acd956ef733311c9",
+    "url": "/coldzify1/react-demo/static/css/main.93bbad74.chunk.css"
+  },
+  {
+    "revision": "3935589186760b313307",
+    "url": "/coldzify1/react-demo/static/js/2.fe08795f.chunk.js"
+  },
+  {
+    "revision": "7f07acd956ef733311c9",
+    "url": "/coldzify1/react-demo/static/js/main.5edace07.chunk.js"
+  },
+  {
+    "revision": "af002eb3f1816d1c6f59",
+    "url": "/coldzify1/react-demo/static/js/runtime~main.74bd80ea.js"
+  }
+]);
